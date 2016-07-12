4697f146ea5a65105dfaa8a6ed32451cd81c4d27
ezlee <jerry2096@hotmail.com>
2016-07-12 20:55:54 +0000

    add firewalld, change to vagrant/sudo user

--- roles/nginx/spec/nginx_spec.rb
+++ roles/nginx/spec/nginx_spec.rb
@@ -9,6 +9,16 @@ describe service('nginx') do
   it { should be_running   }
 end
 
+describe package('FirewallD') do
+  it { should be_installed }
+end
+
+
+describe service('FirewallD') do
+  it { should be_enabled   }
+  it { should be_running   }
+end
+
 describe port(property['www_port']) do
   it { should be_listening }
 end
